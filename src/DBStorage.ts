a81c3463d7bf7da0a53c9c56ee595fe419c12e09
@dimanadko <dimanadobko@gmail.com>
2020-12-26 23:59:53 +0200

    feat: implement test db func

--- src/DBStorage.ts
+++ src/DBStorage.ts
@@ -1,3 +1,4 @@
+const format = require( 'pg-format');
 const { Client } = require('pg');
 
 
@@ -13,5 +14,43 @@ client
     .connect()
     .then(() => console.log('Connected to DB'))
     .catch((err: Error) => console.error('Connection DB error', err.stack));
+
+
+
+const writeToDb = () => {
+    const values = [
+        ['user1'],
+        ['user2']
+    ];
+
+    const valuesContr = [
+        [new Date().toISOString(), 'test', 'typo_editting'],
+        [new Date().toISOString(), 'test2', 'content_addition']
+    ];
+    client.query(
+        format('INSERT INTO users (username) VALUES %L', values),
+        [],
+        (err: Error, result: any) => {
+            console.log(err);
+            console.log(result);
+        }
+    );
+    client.query(
+        format('INSERT INTO contributions (timestamp, topic, contribution_type) VALUES %L', valuesContr),
+        [],
+        (err: Error, result: any) => {
+            console.log(err);
+            console.log(result);
+        }
+    );
+};
+
+writeToDb();
+
+export {
+    writeToDb
+}
+
+
 
 export default client;
