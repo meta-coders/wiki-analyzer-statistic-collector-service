a01ab3f918209a7c82ca773647c8f757c1a5bd6e
@dimanadko <dimanadobko@gmail.com>
2020-12-26 23:07:59 +0200

    feat: implement event stream processing

--- src/collector.ts
+++ src/collector.ts
@@ -1,21 +1,58 @@
-import Websocket from 'ws';
-import DBclient from './DBStorage';
-import path from 'path';
+import Websocket, {MessageEvent} from "ws";
+import DBclient from "./DBStorage";
+import path from "path";
+import DetailedWikiEvent from "./interfaces/DetailedWikiEvent";
+import {defer, fromEvent, Observable} from "rxjs";
+import {map, pluck, tap} from "rxjs/operators";
+import {stringify} from "querystring";
+import concurrentConcat from "./utils/ConcurrentConcat";
+
+const ws = new Websocket("ws://localhost:3000/detailed-recent-changes");
+
+ws.on(
+    "open",
+    () => {
+        ws.send(new Date().toISOString());
+        console.log("Connected to Recent Changes Service");
+        // ws.close();
+    }
+);
+
+//
+// ws.on(
+//     'message',
+//     async (data: string) => {
+//         const message = JSON.parse(data);
+//         console.log(message)
+//     }
+// );
+
+const testPromise = async(): Promise<String> => {
+    return 'sss';
+};
 
 
-const ws = new Websocket('ws://localhost:3000/recent-changes');
 
-ws.on('open', () => {
-  console.log('Connected to Recent Changes Service');
-  ws.close();
-});
+const eventStream = fromEvent(ws, 'message')
+    .pipe(
+        pluck('data'),
+        map((message: any): DetailedWikiEvent => JSON.parse(message) as DetailedWikiEvent)
+    // ).subscribe(function(b) { console.log(b); });
+    ).pipe(
+        concurrentConcat(
+            (event: DetailedWikiEvent): Observable<String> => {
+                return defer(() =>
+                    testPromise()
+                )
+            }
+        )
+    ).subscribe(function(b) { console.log(b); });
 
-// ws.onmessage()
 
 DBclient.query("SELECT * FROM contributions",
-  (err: Error, res: String) => {
-    if (err) throw err;
-    // console.log(res);
-    DBclient.end()
-  }
+    (err: Error, res: String) => {
+        if (err) throw err;
+        // console.log(res);
+        DBclient.end();
+    }
 );
