9a57a05f2861506823daef60da391f1e8c4de19e
@dimanadko <dimanadobko@gmail.com>
2020-12-30 00:38:51 +0200

    fix: retry polling service connection

--- src/utils/retry-backoff.ts
+++ src/utils/retry-backoff.ts
@@ -2,27 +2,27 @@ import { MonoTypeOperatorFunction, Observable, throwError, timer } from 'rxjs';
 import { concatMap, retryWhen } from 'rxjs/operators';
 
 export default function retryBackoff<T>(
-    maxRetries: number,
-    timeout: number,
-    tag: string,
+  maxRetries: number,
+  timeout: number,
+  tag: string,
 ): MonoTypeOperatorFunction<T> {
-    return (input: Observable<T>) => {
-        return input.pipe(
-            retryWhen((errors) => {
-                return errors.pipe(
-                    concatMap((error, attempt) => {
-                        const retryAttempt = attempt + 1;
+  return (input: Observable<T>) => {
+    return input.pipe(
+      retryWhen((errors) => {
+        return errors.pipe(
+          concatMap((error, attempt) => {
+            const retryAttempt = attempt + 1;
 
-                        if (retryAttempt > maxRetries) {
-                            return throwError(error);
-                        }
+            if (retryAttempt > maxRetries) {
+              return throwError(error);
+            }
 
-                        console.log(`[${tag}]: ${error}, Attempt: ${retryAttempt}`);
+            console.log(`[${tag}]: ${error}, Attempt: ${retryAttempt}`);
 
-                        return timer(timeout);
-                    }),
-                );
-            }),
+            return timer(timeout);
+          }),
         );
-    };
+      }),
+    );
+  };
 }
